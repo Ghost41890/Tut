1aa6879475eb4eef5c2defeb8107cedfd4cb3ed7
Ghost41890 <avanish2001bhosale@gmail.com>
2021-06-30 17:16:04 +0530

    kk

---
+++ tut1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+//ok
+int main()
+{
+   int num1,num2; 
+    cout<<"Enter the value of num1:/ n"; /* << is called insertion operator */
+    cin>>num1;; /* << is called extraction operator */
+    
+    cout<<"Enter the value of num2:/ n"; /* << is called insertion operator */
+    cin>>num1;; /* << is called extraction operator */
+
+    cout<<"The sum is "<<num1+num2;
+    return 0;
+}
+
